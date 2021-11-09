957b6be29318091b67926ffc066523ee86f92a7a
GhzGarage <gigahertzgarage@mail.com>
2021-11-09 01:06:04 -0600

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # qb-target
+
+Just an upload before the name change and standalone stuff. Don't ask me for support
---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BerkieB & Contributors'
+description 'A better interaction for FiveM, a third eye for you to interact with and keep your server optimised!'
+version '3.1.4'
+
+ui_page 'html/index.html'
+
+client_scripts {
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/EntityZone.lua',
+	'@PolyZone/CircleZone.lua',
+	'@PolyZone/ComboZone.lua',
+	'client/*.lua',
+}
+
+files {
+	'config.lua',
+	'html/*.html',
+	'html/css/*.css',
+	'html/js/*.js'
+}
+
+dependencies {
+	'PolyZone',
+	'qb-core'
+}
